4d6d243997543ec805419c68de9c59e3f8b58137
moeka kitamura <moeka.k76@gmail.com>
2021-08-15 08:14:35 +0900

    set controller, change routing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+    protect_from_forgery with: :exception
+    include SessionsHelper
+    before_action :login_required
+    private
+    def login_required
+        redirect_to new_session_path, alert: "⚠️You have to log in." unless current_user
+    end
+    def already_logged_in
+        redirect_to pictures_path, notice: "You are already logged in." if current_user
+    end
+    def  same_user?
+        @picture.user_id == current_user.id
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,17 @@ Rails.application.routes.draw do
   root 'users#new'
   resources :pictures do
     collection do
-      post :confirm, :favorite
+      post :confirm
+    end
+    member do
+      get :favorite
     end
   end
-  resources :users, except: [:index] 
+  resources :users, except: [:index] do
+    member do
+      get :favorite
+    end
+  end
 end
 
 # resources :sessions, only: [:new, :create, :destroy]
