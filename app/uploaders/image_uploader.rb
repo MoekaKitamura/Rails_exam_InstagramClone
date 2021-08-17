e9560dbb6b40e0fa6f28ff45d108c04e3143d40b
moeka kitamura <moeka.k76@gmail.com>
2021-08-17 15:47:00 +0900

    show current icon on edit page, add icon on index page

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,7 +1,22 @@
 class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
   storage :file
-  process :resize_to_fill => [320, 320]
+  process :resize_to_fill => [350, 350]
+
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  #デフォルト画像の設定
+  # def default_url(*args)
+    # "default.png"
+  # end
+end
+
+class IconUploader < CarrierWave::Uploader::Base
+  include CarrierWave::MiniMagick
+  storage :file
+  process :resize_to_fill => [180, 180]
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
