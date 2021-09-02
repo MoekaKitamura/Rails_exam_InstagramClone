2097eeb8f7cc2a3ecf5ea3b8ab7f2bd396761d09
moeka kitamura <moeka.k76@gmail.com>
2021-09-02 13:40:52 +0900

    create relationship model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,10 @@ class User < ApplicationRecord
     has_many :pictures, dependent: :destroy
     has_many :favorites, dependent: :destroy
     has_many :favorite_pictures, through: :favorites, source: :picture
-
+    has_many :active_relationships, foreign_key: 'follower_id', class_name: 'Relationship', dependent: :destroy
+    has_many :passive_relationships, foreign_key: 'followed_id', class_name: 'Relationship', dependent: :destroy
+    has_many :following, through: :active_relationships, source: :followed
+    has_many :followers, through: :passive_relationships, source: :follower
 
     validates :name,  presence: true, length: { maximum: 30 }
     validates :email, presence: true, length: { maximum: 255 },format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
