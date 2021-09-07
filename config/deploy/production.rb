17b924e94c7aab0f64d483192eb59102785276b9
moeka kitamura <moeka.k76@gmail.com>
2021-09-07 22:41:14 +0900

    edit production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,2 +1,2 @@
-server '35.72.143.209', user: 'moeka', roles: %w{moeka db web}
+server '35.72.143.209', user: 'moeka', roles: %w{app db web}
 set :ssh_options, keys: '/Users/kitamuramoeka/.ssh/id_rsa'
